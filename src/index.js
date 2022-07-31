2cc3d07f736bd573844d1f5867da862f35d6241f
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-07-31 09:22:32 +0200

    Styling

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetails from "./CommentDetails";
 import ApprovalCard from "./ApprovalCard";
+import "./style/App.css";
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard>
+        <h4>warning!</h4>
+        Are you sure you want to do this?
+      </ApprovalCard>
       <ApprovalCard>
         {" "}
         <CommentDetails
