814b8b6d46148859a8c9269d12f8e665448537d6
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-07-31 09:15:40 +0200

    ApprovalCards

--- src/index.js
+++ src/index.js
@@ -6,25 +6,32 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetails
-        author="gonzalo"
-        timeAgo="Yesterday"
-        content="Nice Topic!"
-        img={faker.image.image()}
-      />
-      <CommentDetails
-        author="adham"
-        timeAgo="Today"
-        content="It is brilliant!"
-        img={faker.image.image()}
-      />
-      <CommentDetails
-        author="hossam"
-        timeAgo="2 days ago"
-        content="Woow!"
-        img={faker.image.image()}
-      />
+      <ApprovalCard>
+        {" "}
+        <CommentDetails
+          author="gonzalo"
+          timeAgo="Yesterday"
+          content="Nice Topic!"
+          img={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        {" "}
+        <CommentDetails
+          author="adham"
+          timeAgo="Today"
+          content="It is brilliant!"
+          img={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails
+          author="hossam"
+          timeAgo="2 days ago"
+          content="Woow!"
+          img={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
